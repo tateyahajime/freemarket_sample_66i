014febdcda06e4253ca203c29933cd6cff27908f
moritakentarou <1993mxxakmkxxm@gmail.com>
2020-01-08 19:19:09 +0900

    brand変更

--- db/migrate/20200108095610_create_brands.rb
+++ db/migrate/20200108095610_create_brands.rb
@@ -2,7 +2,7 @@ class CreateBrands < ActiveRecord::Migration[5.2]
   def change
     create_table :brands do |t|
       t.string :brands_name
-      t.references :items_id
+      t.references :items foreign_key: true
       t.timestamps
     end
   end
