e07bc96ad9cdd8df089471b3cb71c9657b1acc3b
sasamoto-miyu <miyu0908ssmt@gmail.com>
2020-01-08 21:25:39 +0900

    追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_08_092232) do
+ActiveRecord::Schema.define(version: 2020_01_08_095610) do
+
+  create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "brands_name"
+    t.bigint "items_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["items_id"], name: "index_brands_on_items_id"
+  end
 
   create_table "x_categroys", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "z_category_name"
