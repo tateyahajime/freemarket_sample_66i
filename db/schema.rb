0222e7a1731001eb390341b2566fb75f0c28403b
sasamoto-miyu <miyu0908ssmt@gmail.com>
2020-01-15 10:38:24 +0900

    Basic認証

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_01_14_115540) do
+ActiveRecord::Schema.define(version: 2020_01_14_123741) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "prefecture_id"
