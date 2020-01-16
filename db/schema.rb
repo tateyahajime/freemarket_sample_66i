9a34a4c4d40fa366d89898d447c5c9e1eae674bb
tateyahajime <kuruhina0228@gmail.com>
2020-01-16 12:42:20 +0900

    出品機能１

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_01_14_115540) do
+ActiveRecord::Schema.define(version: 2020_01_14_123741) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "prefecture_id"
@@ -130,6 +129,12 @@ ActiveRecord::Schema.define(version: 2020_01_14_115540) do
 
   create_table "x_categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "x_category_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "x_categroys", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "z_category_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
